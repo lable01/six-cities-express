f28f732ace32cc76529ac37c9d9eb9081aa3c3fd
Konstanin <lable_01@mail.ru>
2024-10-01 22:47:38 +0300

    добавил функцию generate

--- src/cli/commands/generate.ts
+++ src/cli/commands/generate.ts
@@ -1,15 +1,47 @@
+import got from 'got';
+import { MockServerData } from '../../shared/types/mock-server-data.js';
 import { Command } from '../../shared/interface/index.js';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/index.js';
+import { appendFile } from 'node:fs/promises';
 
 export class Generate implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
+        encoding: 'utf8',
+      });
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера.
-    // Формирование объявлений.
+    try {
+      await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created!`);
+    } catch (error: unknown) {
+      console.error(`Can't generate data`); // eslint-disable-line quotes
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
